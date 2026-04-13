4deb3a11de60086280ee043252423173a68b09dc
esereskicirak <eser.eskicirak@gmail.com>
2026-04-13 17:17:52 +0300

    Update [jobId].js

--- api/crawl/status/[jobId].js
+++ api/crawl/status/[jobId].js
@@ -1,3 +1,10 @@
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+if (req.method === "OPTIONS") {
+  return res.status(200).end();
+}
 export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
